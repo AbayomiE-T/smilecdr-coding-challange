4c3642bb1918f078b4c231ff2a0da5889df2f88d
Abayomi <eyitouyoa3@gmail.com>
2022-03-18 14:34:07 -0400

    created patients components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PatientListComponent } from './components/patient-list/patient-list.component';
+import { PatientComponent } from './components/patient/patient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientListComponent,
+    PatientComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/patient.service.ts
+++ src/app/services/patient.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PatientService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getPatients(): Observable<any> {
+    const baseUrl = 'https://try.smilecdr.com/baseR4/Patient'
+
+    return this.http.get(baseUrl);
+  }
 }
